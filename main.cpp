230e6c54e48e1dd873035a52b1baf9deba24cc10
GatoHery <00177919@uca.edu.sv>
2023-05-12 01:14:21 -0600

    agregar funcionalidades de excel
    -se puede usar la mayoria de funciones para un excel 5x5
    -funcion guardar no realiza ninguna accion

--- main.cpp
+++ main.cpp
@@ -6,16 +6,16 @@ using namespace::std;
 //menus interactivos
 void menu_tamaño(); //pregunta si el tamano del excel es adecuado para el trabajo
 void menu_principal();
-void menu_funciones();
+void menu_funciones(string id);
 
 //funciones de apoyo
-void posicion(string ubicacion);
 
 int op = 0;
 int opAux = 0;
 int cont = 0;
+
+string id="";
 string dato= "";
-string ubicacion = "";
 string portapapeles = "";
 
 int main(){
@@ -28,65 +28,165 @@ int main(){
             if(op == 1){
                 return 0;
             }
-            else if(op == 2){
+            else if(op == 2){ //mantener 5x5
                 cont = 5;
-                for(int i=0; i<cont;i++) {
+                for(int i=0; i<cont;i++) { //se insertan los nodos con datos
                     dato = "|1| ";
-                    insert_front(&head, dato);
+                    insertar(&head, dato);
                     dato = "|2| ";
-                    insert_front(&head2, dato);
+                    insertar(&head2, dato);
                     dato = "|3| ";
-                    insert_front(&head3, dato);
+                    insertar(&head3, dato);
                     dato = "|4| ";
-                    insert_front(&head4, dato);
+                    insertar(&head4, dato);
                     dato = "|5| ";
-                    insert_front(&head5, dato);
+                    insertar(&head5, dato);
                 }
-                cout<<endl;
-                displayList(head);
-                cout<<endl;
-                displayList(head2);
-                cout<<endl;
-                displayList(head3);
-                cout<<endl;
-                displayList(head4);
-                cout<<endl;
-                displayList(head5);
 
+                //se ponen el ID, lo importante es la letra, el numero se cambia dentro de la funcion
+                id="A0";
+                agregarId(head,id);
+                id="B0";
+                agregarId(head2,id);
+                id="C0";
+                agregarId(head3,id);
+                id="D0";
+                agregarId(head4,id);
+                id="E0";
+                agregarId(head5,id);
+                id="A1";
 
+                //todas las acciones que realizara el usuario
                 do{
+                    //depliega toda la lista de 5x5
                     cout<<endl;
-                    menu_funciones();
+                    desplegarLista(head);
+                    cout<<endl;
+                    desplegarLista(head2);
+                    cout<<endl;
+                    desplegarLista(head3);
+                    cout<<endl;
+                    desplegarLista(head4);
+                    cout<<endl;
+                    desplegarLista(head5);
+                    cout<<endl;
+
+                    menu_funciones(id);
                     cin>>opAux;
                     switch (opAux) {
                         case 1: //escribir en una celda
                             cout<<"digite el dato"<<endl;
                             cin>>dato;
-                            if(dato.length() == 8){
-                                cout<<"se ha escrito el dato"<<endl;
-                                //accion de meter el valor (pendiente)------------------------
+                            if(dato.length() <= 8){
+                                switch (id[0]) {
+                                    case 'A':
+                                        buscarId(head,id,dato);
+                                        break;
+                                    case 'B':
+                                        buscarId(head2,id,dato);
+                                        break;
+                                    case 'C':
+                                        buscarId(head3,id,dato);
+                                        break;
+                                    case 'D':
+                                        buscarId(head4,id,dato);
+                                        break;
+                                    case 'E':
+                                        buscarId(head5,id,dato);
+                                        break;
+                                    default:
+                                        cout<<"error en el ID"<<endl;
+                                }
                             }
                             else{
                                 cout<<"el dato ingresado es mas grande de lo permitido"<<endl;
                             }
                             break;
-                        case 2: //posicionarse en una celda
+                        case 2: //posicionarse en una celda----------------
                         cout<<"escriba la ubicacion"<<endl;
-                        cin>>ubicacion;
-                        if(ubicacion.length() == 2){
-                            //agregar accion de navegacion entre nodos----------------------
-                            posicion(ubicacion);
+                        cin>>id;
+                        if(id.length() == 2){
+                            id[0] = toupper(id[0]);
                         }
                         else{
                             cout<<"tamano no valido"<<endl;
+                            id="A1";
                         }
                             break;
-                        case 3: //copiar contenido de la celda
+                        case 3: //copiar contenido de la celda-----------------------
+                            switch (id[0]) {
+                                case 'A':
+                                     portapapeles = buscarId(head,id);
+                                     cout<<"contenido copiado: "<<portapapeles<<endl;
+                                    break;
+                                case 'B':
+                                    portapapeles = buscarId(head2,id);
+                                    break;
+                                case 'C':
+                                    portapapeles = buscarId(head3,id);
+                                    break;
+                                case 'D':
+                                    portapapeles = buscarId(head4,id);
+                                    break;
+                                case 'E':
+                                    portapapeles = buscarId(head5,id);
+                                    break;
+                                default:
+                                    cout<<"error en el ID"<<endl;
+                            }
                             break;
-                        case 4: //cortar contenido de la celda
+                        case 4: //cortar contenido de la celda------------------
+                            switch (id[0]) {
+                                case 'A':
+                                    portapapeles = cortarDato(head,id);
+                                    cout<<"contenido cortado: "<<portapapeles<<endl;
+                                    break;
+                                case 'B':
+                                    portapapeles = cortarDato(head2,id);
+                                    break;
+                                case 'C':
+                                    portapapeles = cortarDato(head3,id);
+                                    break;
+                                case 'D':
+                                    portapapeles = cortarDato(head4,id);
+                                    break;
+                                case 'E':
+                                    portapapeles = cortarDato(head5,id);
+                                    break;
+                                default:
+                                    cout<<"error en el ID"<<endl;
+                            }
                             break;
-                        case 5: // pegar contenido
-
+                        case 5: // pegar contenido---------------------------
+                            if(portapapeles != ""){
+                                switch (id[0]) {
+                                    case 'A':
+                                        pegarDato(head,id,portapapeles);
+                                        cout<<"contenido pegado: "<<portapapeles<<endl;
+                                        break;
+                                    case 'B':
+                                        pegarDato(head,id,portapapeles);
+                                        cout<<"contenido pegado: "<<portapapeles<<endl;
+                                        break;
+                                    case 'C':
+                                        pegarDato(head,id,portapapeles);
+                                        cout<<"contenido pegado: "<<portapapeles<<endl;
+                                        break;
+                                    case 'D':
+                                        pegarDato(head,id,portapapeles);
+                                        cout<<"contenido pegado: "<<portapapeles<<endl;
+                                        break;
+                                    case 'E':
+                                        pegarDato(head,id,portapapeles);
+                                        cout<<"contenido pegado: "<<portapapeles<<endl;
+                                        break;
+                                    default:
+                                        cout<<"error en el ID"<<endl;
+                                }
+                            }
+                            else{
+                                cout<<"Tu portapapeles esta vacio, imposible realizar esta accion"<<endl;
+                            }
                             break;
                         case 6: //guardar archivo
                             cout<<"guardando archivo..."<<endl;
@@ -131,7 +231,12 @@ void menu_principal(){
     cout<<"respuesta: ";
 }
 
-void menu_funciones(){
+void menu_funciones(string id){
+    cout<<"--------------------------------"<<endl;
+    if(portapapeles != ""){
+        cout<<"Clipboard: "<<portapapeles<<endl;
+    }
+    cout<<"posicion actual: "<<id<<endl;
     cout<<"------Menu de acciones----------"<<endl;
     cout<<"1. Escribir en una celda"<<endl;
     cout<<"2. Posicionarse en una celda"<<endl;
@@ -141,19 +246,4 @@ void menu_funciones(){
     cout<<"6. Guardar hoja de calculo"<<endl;
     cout<<"0. Salir de la hoja de calculo"<<endl;
 }
-//----------funciones-----------
-void posicion(string ubicacion){ //buscar entre todos los nodos hasta encontrar el id correcto
-    cout<<"ubicacion actual: "<<ubicacion<<endl;
-}
 
-void copiar(){
-
-}
-
-void cortar(){
-
-}
-
-void pegar(){
-
-}
--- nodo.h
+++ nodo.h
@@ -2,6 +2,7 @@
 using namespace std;
 
 struct Node {
+    string id = "0";
     string data;
     struct Node* next;
     struct Node* prev;
@@ -13,7 +14,7 @@ struct Node* head3 = NULL;
 struct Node* head4 = NULL;
 struct Node* head5 = NULL;
 
-void insert_front(struct Node** head, string new_data)
+void insertar(struct Node** head, string new_data)
 {
     //allocate memory for New node
     struct Node* newNode = new Node;
@@ -33,7 +34,7 @@ void insert_front(struct Node** head, string new_data)
     (*head) = newNode;
 }
 
-void displayList(struct Node* node) {
+void desplegarLista(struct Node* node) {
     struct Node* last;
 
     while (node != NULL) {
@@ -42,3 +43,82 @@ void displayList(struct Node* node) {
         node = node->next;
     }
 }
+
+void buscarId(struct Node* node,string id,string dato) {
+    struct Node* last;
+
+    while (node != NULL) {
+        if(node->id == id){
+            node->data = dato;
+            cout<<"se ha escrito el dato"<<endl;
+            last = node;
+            node = node->next;
+        }
+        else{
+            last = node;
+            node = node->next;
+        }
+    }
+}
+//borrar el dato almacenado para pasarlo al clipboard
+string cortarDato(struct Node* node,string id) {
+    struct Node* last;
+    string aux="";
+
+    while (node != NULL) {
+        if(node->id == id){
+            cout<<"dato cortado"<<endl;
+            aux = node->data;
+            node->data = "|-| ";
+            return aux;
+        }
+        else{
+            last = node;
+            node = node->next;
+        }
+    }
+    return "";
+}
+
+void pegarDato(struct Node* node,string id,string dato) {
+    struct Node* last;
+
+    while (node != NULL) {
+        if(node->id == id){
+            cout<<"dato pegado"<<endl;
+            node->data = dato;
+        }
+        else{
+            last = node;
+            node = node->next;
+        }
+    }
+}
+
+string buscarId(struct Node* node,string id) {
+    struct Node* last;
+
+    while (node != NULL) {
+        if(node->id == id){
+            cout<<"dato copiado"<<endl;
+            return node->data;
+        }
+        else{
+            last = node;
+            node = node->next;
+        }
+    }
+    return "";
+}
+
+void agregarId(struct Node* node,string id) {
+    struct Node* last;
+    int cont = 1;
+    while (node != NULL) {
+        id[1] = cont + '0';
+            node->id= id;
+            last = node;
+            node = node->next;
+            cont+=1;
+            }
+    }
